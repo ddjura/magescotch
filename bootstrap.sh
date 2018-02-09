754d834452b5d31e9af656109888fd1685daeb89
Marko Durasic <marko_djurasic@hotmail.com>
2018-02-09 18:27:01 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 chown vagrant:vagrant /home/vagrant/.composer/auth.json
 chown -R vagrant:vagrant /home/vagrant
 
+mkdir -p /var/www/public/
 cp -r /opt/magento1 /var/www/public/
 cp -r /opt/magento2 /var/www/public/
 
