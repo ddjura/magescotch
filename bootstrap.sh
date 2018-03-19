f693a05212389f8b392fd9dc963d58d95347c0c5
Marko Durasic <marko_djurasic@hotmail.com>
2018-03-19 12:32:18 +0800

    add commands to disable/enable Xdebug
    Xdebug slows performance therefore when not debugging just disable it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,10 @@ cp -r /opt/magento2 /var/www/public/
 chown -R vagrant:vagrant /var/www/public/magento2/
 chmod -R 777 /var/www/public/magento2/
 
+#add commands to disable/enable Xdebug when not debugging because it increases performance
+echo "alias disablexdebug='sudo phpdismod xdebug &&  sudo service apache2 restart && sudo service php7.0-fpm restart'" >> /home/vagrant/.bashrc
+echo "alias enablexdebug='sudo phpenmod xdebug &&  sudo service apache2 restart && sudo service php7.0-fpm restart'" >> /home/vagrant/.bashrc
+source ~/.bashrc
 # cd /var/www/public/magento1/
 # chmod -R a+rw * 
 # modman init
